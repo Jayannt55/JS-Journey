ed39ff7d3edc5f8b35161338626024fba576359f
Jayant Baliyan <jayantbaliyan909@gmail.com>
2026-02-23 20:28:11 +0530

    datatypes

---
+++ datatypes.js
@@ -0,0 +1,22 @@
+"use strict"; //treat all code as a newer version of js.
+/*alert("hello"); ab yeh browser ke console me chal jayega per yeh node js run nhi karega */
+console.log("jayant");
+console.log(
+  3 + 3,
+); /*ab js m hum yeh semi colon lagau ya nhi sab js apne aap kar leta h per yeh code readbility khrab karta h isliye recommended h ki lagao or jo dusra console.log usko bhi next line likho */
+let name = "jayant"
+let age = 23
+let isLoggedIn = true
+
+// number = 2 to power of 53
+// bigint -> for big number 
+// string -> ""
+// boolean = true or false 
+// null -> standalone value h 
+// undefined -> means mene bas variable diya per usme koi value nhi diya h 
+// symbol => unique 
+
+// object 
+
+console.log(typeof "jayant")
+console.log(typeof null)
